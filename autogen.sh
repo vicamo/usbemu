3f56d7a896b6ea3f3f6f5a4fe0484f24930f5788
You-Sheng Yang <vicamo@gmail.com>
2016-02-23 02:48:51 +0800

    doc: add gtk-doc support
    Signed-off-by: You-Sheng Yang <vicamo@gmail.com>

--- autogen.sh
+++ autogen.sh
@@ -8,6 +8,14 @@ cd "$srcdir"
 
 mkdir -p m4
 
+GTKDOCIZE=`which gtkdocize`
+if test -z $GTKDOCIZE; then
+  echo "*** No GTK-Doc found, please install it ***"
+  exit 1
+else
+  gtkdocize || exit $?
+fi
+
 # README and INSTALL are required by automake, but may be deleted by clean
 # up rules. to get automake to work, simply touch these here, they will be
 # regenerated from their corresponding *.in files by ./configure anyway.
